801a488289aa34032eb6df5e0170e88254b6f0f5
Daniel Walton <dwaltonjr9425@gmail.com>
2022-04-13 12:05:04 -0700

    removed unused

--- pages/works.js
+++ pages/works.js
@@ -11,7 +11,6 @@ import thumbModeTokyo from '../public/images/works/modetokyo_eyecatch.png'
 import thumbStyly from '../public/images/works/styly_eyecatch.png'
 import thumbPichu2 from '../public/images/works/pichu2_eyecatch.png'
 import thumbFreeDBTagger from '../public/images/works/freedbtagger_eyecatch.png'
-import thumbAmembo from '../public/images/works/amembo_eyecatch.png'
 import thumbDaxon from '../public/images/works/daxon_eyecatch.png'
 const Works = () => (
   <Layout title="Works">
